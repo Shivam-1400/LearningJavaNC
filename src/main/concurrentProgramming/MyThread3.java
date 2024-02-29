e6ad921be5798cb93195a48f73504aba75a0815b
Shivam Keshri <skshivam.keshri852@gmail.com>
2024-02-29 15:08:57 +0530

    Update MyThread3.java

--- src/main/concurrentProgramming/MyThread3.java
+++ src/main/concurrentProgramming/MyThread3.java
@@ -12,15 +12,19 @@ class PrintTable2 implements Runnable{
         System.out.println("Creating thread "+ name);
     }
     public void run(){
-        System.out.println("Running Thread "+ name+"->"+"table of "+ num+" from 1 to "+ ed+ Thread.currentThread()+"---");
+        System.out.println("Running Thread "+ name+"->"+ " from 1 to "+ ed+ Thread.currentThread()+"---");
         for(int i=1; i<= ed; i++){
             System.out.println(num+"*"+i+"="+(num*i));
             try {
                 Thread.sleep(1000);
+                while (suspended){
+                    wait();
+                }
             } catch (InterruptedException e) {
                 System.out.println(e);
             }
         }
+        System.out.println("Thread " +  t.getName() + " exiting.");
     }
     void start(){
         System.out.println("Starting thread "+ name);
@@ -28,6 +32,13 @@ class PrintTable2 implements Runnable{
             t= new Thread(this, name);
             t.start();
         }
+    }
+    void suspend(){
+        suspended= true;
+    }
+    synchronized void resume(){
+        suspended= false;
+        notify();
     }
 }
 
@@ -39,5 +50,25 @@ public class MyThread3 {
         t1.start();
         t2.start();
 
+        try {
+            Thread.sleep(1000);
+            t1.suspend();
+            System.out.println("thred "+ t1.name+" suspended");
+            Thread.sleep(100);
+            t1.resume();
+            System.out.println("thread "+ t1.name+ " resumed");
+
+
+            Thread.sleep(1000);
+            t2.suspend();
+            System.out.println("thred "+ t2.name+" suspended");
+            Thread.sleep(100);
+            t2.resume();
+            System.out.println("thread "+ t2.name+ " resumed");
+
+
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        }
     }
 }
