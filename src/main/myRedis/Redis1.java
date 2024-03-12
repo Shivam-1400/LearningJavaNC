4dd5f045423731bfe7e05995ac83eddc10673040
Shivam Keshri <skshivam.keshri852@gmail.com>
2024-03-12 11:06:40 +0530

    updates

--- src/main/myRedis/Redis1.java
+++
@@ -1,8 +0,0 @@
-package main.myRedis;
-
-public class Redis1 {
-    public static void main(String [] args){
-        System.out.println("Hello world");
-//        Jedis jedis = new Jedis();
-    }
-}
